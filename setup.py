8a9e0e09fe5b794f8b65989e237036df520e6ffb
twj2417 <twj2417@163.com>
2018-08-19 14:47:40 +0800

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='jfs',
-    version='0.0.1',
+    version='0.1.0',
     description='File Processing Library.',
     url='https://github.com/twj2417/filesystem',
     author='Weijie Tao',
